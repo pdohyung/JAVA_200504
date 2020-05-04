c11be4d46182c71ab1b3bb125803f70d203667df
pdohyung <58456758+pdohyung@users.noreply.github.com>
2020-05-04 23:39:33 +0900

    Create DataType.java

---
+++ DataType.java
@@ -0,0 +1,30 @@
+package project;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        System.out.println(7); // Number
+        System.out.println("seven"); // String
+        
+        System.out.println("7"); // String 6
+        
+        System.out.println(7+7); // 12
+        System.out.println("7" + "7"); //66
+        
+        System.out.println(7*7);
+       // System.out.println("7"*"7");
+        
+        System.out.println("1111".length());
+       // System.out.println(1111.length());
+        
+        System.out.println("Hello World"); //String 문자열
+        System.out.println('H'); //Char 문자 
+        System.out.println("H");
+    }
+
+}
